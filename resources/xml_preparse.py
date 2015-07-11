843f802628e3f0db57bf4949afc2e4d8cbb4f2f5
Eric Gan <eric@ercgn.com>
2015-07-11 01:37:34 -0700

    Added stemmed tokenized words

--- resources/xml_preparse.py
+++ resources/xml_preparse.py
@@ -4,18 +4,26 @@
 
 import xml.etree.ElementTree as ET
 from nltk.corpus import stopwords
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.tokenize import RegexpTokenizer
 
 if __name__ == '__main__':
     tree = ET.parse("ESV_processed.xml")
     bibleRoot = tree.getroot()
+    st = LancasterStemmer()
+    tokenizer = RegexpTokenizer(r'\w+')
    
     for book in bibleRoot:
         print book.get("n")
         for chapter in book:
             for verse in chapter: 
                 newTextArr = filter(
-                    lambda w: w.lower() not in stopwords.words("english"), verse.text.split(" "))
-                verse.text = " ".join(newTextArr)
+                    lambda w: w.lower() not in stopwords.words("english"), 
+                    tokenizer.tokenize(verse.text))
+                stemmedTextArr = map(
+                    lambda w: st.stem(w), 
+                    newTextArr)
+                verse.text = " ".join(stemmedTextArr)
                 verse.set("updated", "yes")
 
     tree.write("ESV_processed.xml")
